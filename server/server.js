007d8b23a22e283986b6af1023594c7b52d61d40
Neha27Gupta5 <124888471+Neha27Gupta5@users.noreply.github.com>
2024-08-12 23:42:46 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ const corsOptions = {
   origin: "http://localhost:5173",
   methods: "GET,POST,PUT,DELETE",
   credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"] 
 }
 app.use(cors(corsOptions));
 
